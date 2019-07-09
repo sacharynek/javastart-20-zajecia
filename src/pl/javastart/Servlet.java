06e8e27362ffc70f2d4f2555a84997a8c1f34268
tomaszlach <tomaszlach86@gmail.com>
2019-07-09 19:28:03 +0200

    Initial commit

---
+++ src/pl/javastart/Servlet.java
@@ -0,0 +1,41 @@
+package pl.javastart;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calculator")
+public class Servlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Post");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String number1 = request.getParameter("number1");
+        String number2 = request.getParameter("number2");
+        System.out.println(number1 + number2);
+
+        String fname = request.getParameter("firstName");
+        String lname = request.getParameter("lastName");
+        String age = request.getParameter("age");
+
+        //wysyłamy odpowiedź
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello " + number1 + number2);
+        if (null != fname && null != lname && null != age) {
+            writer.println("Cześć " + fname + " Wiem, że masz na nazwisko " + lname + " Kiedyś też miałem " + age + " lat");
+        }
+
+        if (null != fname & null == lname || null == age) {
+            writer.println("Cześć " + fname + " Niestety nie wiem jak masz na nazwisko. Ja tez nie lubie chwalić się wiekiem");
+        }
+    }
+}
